42f9c7a426299ece77a102e96b545fbead52f939
GersonAparecidoLima <gersonaparecido@gmail.com>
2024-09-19 11:05:25 -0300

    Desafio Combate

--- src/Champion.java
+++ src/Champion.java
@@ -24,8 +24,7 @@ public class Champion {
     // Método para retornar o status do Champion
     public String status() {
         if (this.life <= 0) {
-        	return this.name + ": "+ this.life +" de vida (morreu)"            		
-            		+ "";
+        	return this.name + ": " + this.life + " de vida (morreu)";
         } else {
             return this.name + ": " + this.life + " de vida";
         }
